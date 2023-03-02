630765ee3ee190e100d3db30684b63f6b4bae911
Heitor Macedo <71463269+heitor-exe@users.noreply.github.com>
2023-03-02 16:36:35 -0300

    Update Teste.java

--- Teste.java
+++ Teste.java
@@ -3,5 +3,8 @@ public class Teste {
     public static void main(String[] args) {
         System.out.println("teste");
     }
-
+    
+    public void salve() {
+        System.out.println("alterado no git");
+    }
 }
