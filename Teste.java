ba31588865831d969cb27facb18776e9e471a6ae
Heitor <heitorvmacedo@gmail.com>
2023-03-02 16:37:38 -0300

    adicionando salve

--- Teste.java
+++ Teste.java
@@ -1,10 +1,13 @@
 public class Teste {
 
-    public static void main(String[] args) {
-        System.out.println("teste");
-    }
-    
     public void salve() {
         System.out.println("alterado no git");
     }
+
+    public static void main(String[] args) {
+        System.out.println("teste");
+        Teste teste = new Teste();
+        teste.salve();
+    }
+
 }
